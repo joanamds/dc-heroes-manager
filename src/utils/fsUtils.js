d1e54b089a767eede45eed9ecd97fce6bbc34caf
joanamds <joanamdsantos1@gmail.com>
2023-09-01 10:15:09 -0300

    :bug: consertando o path do arquivo para as requisições

--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-const fileName = 'dc_heroes.json';
+const fileName = '../dc_heroes.json';
 const filePath = path.join(__dirname, fileName);
 
 const readHeroes = async () => {
